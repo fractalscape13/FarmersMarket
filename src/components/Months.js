72164d8270187898788e1e1fe34c15fbc9dd0ae0
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-21 16:22:04 -0700

    update methods get input from form to update state and save corresponding produce selection
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>
    Co-authored-by: Miles Bradt <smbradtmichael@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>

--- src/components/Months.js
+++ src/components/Months.js
@@ -7,40 +7,52 @@ class Months extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      month: null
+      value: 0,
+      produce: {}
     };
   }
 
   handleChange = (event) => {
-    this.setState({month: event.target.value})
+    this.setState({ value: event.target.value })
   }
-  
-  render(){
+
+  handleSubmit = (event) => {
+    this.state.produce = availableProduce[this.state.value].selection;
+    event.preventDefault();
+  }
+
+  render() {
     return (
       <React.Fragment>
-        <form>
-          <select id="months" value={this.state.month} onChange={this.handleChange}>
-            <option value="january">January</option>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <select id="months" value={this.state.value} onChange={this.handleChange}>
+              <option value="0">January</option>
+              <option value="1">February</option>
+              <option value="2">March</option>
+              <option value="3">April</option>
+              <option value="4">May</option>
+              <option value="5">June</option>
+              <option value="6">July</option>
+              <option value="7">August</option>
+              <option value="8">September</option>
+              <option value="9">October</option>
+              <option value="10">November</option>
+              <option value="11">December</option>
           </select>
+          </label>
+        <input type="submit" value="Submit" />
         </form>
 
-        <h2>Available produce for </h2>
-      </React.Fragment>
+      <h2>Available produce for </h2>
+      <ProduceDisplayComponent 
+      // state object gets passed through this component to display it
+        produce = {this.state.produce}
+      />
+      </React.Fragment >
     );
   }
 }
 
 export default Months;
 
-{/* <option value="0">January</option>
-  <option value="1">February</option>
-  <option value="2">March</option>
-  <option value="3">April</option>
-  <option value="4">May</option>
-  <option value="5">June</option>
-  <option value="6">July</option>
-  <option value="7">August</option>
-  <option value="8">September</option>
-  <option value="9">October</option>
-  <option value="10">November</option>
-  <option value="11">December</option> */}
